f56abba3ddc90a2ea0f33f7cd5bac947a26390f7
DC-Copeland <52902986+DC-Copeland@users.noreply.github.com>
2019-07-28 22:22:58 -0700

    Default Player Metal Steps
    Players metal footsteps imported in Wwise within switch container>random container. Wwise event and switch set for adaptable triggering via respective materials.

---
+++ jump/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAYER_FOOTSTEPS = 1730208058U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PLAYER_FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 1730208058U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PLAYER_FOOTSTEPS_METAL = 1254697330U;
+                static const AkUniqueID PLAYER_FOOTSTEPS_METAL_ZERO_G = 1191848353U;
+            } // namespace SWITCH
+        } // namespace PLAYER_FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID PLAYER_FOOTSTEPS = 1730208058U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
