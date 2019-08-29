e86de1e6bbf4fe244219daf4a64ef46ba29dc70b
smithDamon <35517446+smithDamon@users.noreply.github.com>
2019-08-29 10:57:54 -0700

    Some fixes to sanity and camera shake, still WIP

--- jump/Assets/Scripts/Hazards/SanityAttackComponent.cs
+++ jump/Assets/Scripts/Hazards/SanityAttackComponent.cs
@@ -7,22 +7,36 @@ public class SanityAttackComponent : MonoBehaviour
     public new Renderer renderer;
     public GameObject player;
 
+    [Tooltip("Intensity of camera shake. keep between 0.1 and 0.2")]
+    public float magnitude;
+
+    [Tooltip("Duration in seconds of camera shake")]
+    public float duration;
+
     public CameraShake camShake;
+
+    [Tooltip("The amount of sanity take from the player each tick")]
     public float sanityLossAmount = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        //renderer = GetComponent<Renderer>();
-        //player = GameObject.Find("Player");
+       renderer = GetComponent<Renderer>();
+       player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //check if the object is rendering
         if (renderer.isVisible)
         {
-            player.GetComponent<playerSanity>().loseSanity(sanityLossAmount);
-            StartCoroutine(camShake.shake(0.15f, 0.4f));
+            //check if player has unobstructed line of sight
+            if (Physics.Linecast(transform.position, player.transform.position))
+            {
+                //apply camera shake and decrease sanity
+                player.GetComponent<playerSanity>().loseSanity(sanityLossAmount);
+                StartCoroutine(camShake.shake(duration, magnitude));
+            }
         }
         if (!renderer.isVisible)
         {
--- jump/Assets/Scripts/Player/playerSanity.cs
+++ jump/Assets/Scripts/Player/playerSanity.cs
@@ -6,28 +6,14 @@ public class playerSanity : MonoBehaviour
 {
     public float sanity = 100;
 
-   //public AudioClip sound;
-   //public AudioClip sound2;
-   //
-   //private AudioSource source;
-    private float volLowRange = .1f;
-    private float volHighRange = 1.0f;
-
-    private float randomTime = 3.0f;
-
     private void Awake()
     {
-        //source = GetComponent<AudioSource>();
     }
 
     public void loseSanity(float sanityLossAmount)
     {
+        Debug.Log("losing sanity");
         sanity -= sanityLossAmount * Time.deltaTime;
-        float vol = Random.Range(volLowRange, volHighRange);
-        //if (!source.isPlaying)
-        //{
-        //    source.PlayOneShot(sound, vol);
-        //}
     }
 
     public void RandomSound()
@@ -37,15 +23,6 @@ public class playerSanity : MonoBehaviour
 
     private void Update()
     {
-        //Randomly play second sound
-        //randomTime -= Time.deltaTime;
-        //if(randomTime <= 0)
-        //{
-        //    if (!source.isPlaying)
-        //    {
-        //        source.PlayOneShot(sound2, 0.5f);
-        //        randomTime = Random.Range(5.0f, 15.0f);
-        //    }
-        //}
+        
     }
 }
